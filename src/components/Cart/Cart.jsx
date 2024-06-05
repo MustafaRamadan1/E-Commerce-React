1c97a8ce15e53e2ae5d76e320e426ae222ee5a4d
MustafaRamadan <Mustafa.ramadan165@gmail.com>
2024-06-05 23:37:33 +0000

    refactor cart design

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,26 +2,52 @@ import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import './Cart.scss';
 function Cart() {
+
+  const products = [{
+    id: 1,
+    img: "https://dmfwoh5tsii9v.cloudfront.net/sites/files/hvv/1.%20Images/IMG_1363.PNG",
+    img2: "https://ae01.alicdn.com/kf/H9ee5dc6fc46b4444b9abf887ccef3df39.jpg_640x640Q90.jpg_.webp",
+    title: "Stylish Summer Dress",
+    description: "A light and breezy summer dress perfect for warm weather. Features a flattering silhouette and vibrant colors.",
+    isNew: true,
+    oldPrice: 59.99,
+    price: 45.99,
+}, {
+    id: 4,
+    img: "https://www.charlestyrwhitt.com/dw/image/v2/AAWJ_PRD/on/demandware.static/-/Sites-ctshirts-master/default/dw97ad9359/SUW0026IDG_MODEL_FULL.jpg?sw=430&sh=538&sm=fit&q=42",
+    img2: "https://www.charlestyrwhitt.com/dw/image/v2/AAWJ_PRD/on/demandware.static/-/Sites-ctshirts-master/default/dw9f2f3eff/hi-res/SUB0506RYL_MODEL_FRONT.jpg?sw=960&sh=1200",
+    title: "Classic Men's Suit",
+    description: "A timeless men's suit that exudes sophistication. Perfect for formal occasions and business settings.",
+    isNew: false,
+    oldPrice: 250.0,
+    price: 199.99,
+}]
+
   return (
     <div className="cart">
       <h3>Products in Your Cart</h3>
-      <div className="cartItem">
-        <div>
-        <h3>Long Sleeve Graphic T-Shirt</h3>
-        </div>
-        <div className="item">
-        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
-          <div className="info">
-          <span className="description">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s
-          </span>
+     {
+      products? products.map((product)=>{
+        return (
+          <div className="cartItem">
+          <div>
+          <h3>{product.title}</h3>
           </div>
-        <DeleteIcon className="delete"/>
+          <div className="item">
+          <img src={product.img} alt={product.title}/>
+            <div className="info">
+            <span className="description">
+              {product.description}
+            </span>
+            </div>
+          <DeleteIcon className="delete"/>
+          </div>
+          <p className="price">1 x ${product.price} </p>
         </div>
-        <p className="price">1 x $99 </p>
-      </div>
+        )
+      }) : null
+     }
+      
     </div>
   );
 }
